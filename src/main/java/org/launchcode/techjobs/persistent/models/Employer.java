7934ec066313168c1d85f6bd6a0cf06b74b56507
Emily Holzman <emily.r.holzman@gmail.com>
2022-10-05 10:24:04 -0400

    part 3 test passed, still working on processAddJobForm in HomeController

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -11,6 +15,10 @@ public class Employer extends AbstractEntity {
     @NotBlank(message = "A location must be provided.")
     @Size(min = 2, max = 50, message = "Location must be between 2 and 50 characters.")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
     public Employer() {}
 
